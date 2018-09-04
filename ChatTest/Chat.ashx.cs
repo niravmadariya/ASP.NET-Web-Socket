2dee7b815ae4abb8e7a6fa5fc715aa0dfb484fe9
niravmadariya <niravmadariya@outlook.com>
2018-09-04 18:28:33 +0530

    ASP.NET Web Socket Example

---
+++ ChatTest/Chat.ashx.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatTest
+{
+    /// <summary>
+    /// Summary description for Chat
+    /// </summary>
+    public class Chat : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+                context.AcceptWebSocketRequest(new WebSocketChatHandler());
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
